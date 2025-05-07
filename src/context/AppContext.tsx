588bba716e238ad7e1788c9cfd9c65490c4264b5
Maarten van den Hooven <maartenv@kontent.ai>
2025-05-07 11:18:57 +0200

    fix retrieving api key

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,11 +1,12 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createContext, FC, PropsWithChildren, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { loadPreviewApiKey } from "../utils/api";
 
 type AppContext = {
   environmentId: string;
   apiKey: string;
-  collection: string;
 };
 
 const defaultAppContext: AppContext = {
@@ -20,6 +21,7 @@ export const useAppContext = () => useContext(AppContext);
 
 export const AppContextComponent: FC<PropsWithChildren> = ({ children }) => {
   const { envId } = useParams();
+  const { getAccessTokenSilently, loginWithRedirect } = useAuth0();
 
   const contextData = useSuspenseQuery({
     queryKey: [`env-data${envId ? `-${envId}` : ""}`],
@@ -27,11 +29,29 @@ export const AppContextComponent: FC<PropsWithChildren> = ({ children }) => {
       if (!envId) {
         return defaultAppContext;
       }
-      return {
-        environmentId: envId,
-        apiKey: import.meta.env.VITE_DELIVERY_API_KEY!,
-        collection: import.meta.env.VITE_COLLECTION!,
-      };
+      return getAccessTokenSilently()
+        .then(res => {
+          return loadPreviewApiKey({
+            accessToken: res,
+            environmentId: envId,
+          });
+        })
+        .then(res => {
+          if (!res) {
+            throw new Error("Could not obtain preview API KEY");
+          }
+
+          return { environmentId: envId, apiKey: res, collection: import.meta.env.VITE_COLLECTION! };
+        })
+        .catch(err => {
+          if (err.error === "login_required") {
+            loginWithRedirect();
+          }
+          if (err.error === "consent_required") {
+            loginWithRedirect();
+          }
+          throw err;
+        });
     },
   });
 
